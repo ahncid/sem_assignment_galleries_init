4e2b47719ec6a7944b0e1d2cad5ec4dce658241e
ahncid <ahncid@gmail.com>
2024-01-29 09:06:52 +0100

    Update page.js

--- src/app/assignments/data/images/page.js
+++ src/app/assignments/data/images/page.js
@@ -1,25 +1,23 @@
-import { fetchImagesForGallery } from '@/lib/data.service';
-import styles from './page.module.css'
-import DevDebugJson from '@/components/dev/devDebugJson/devDebugJson';
-import DevGalleryImage from '@/components/dev/devGalleryImage/devGalleryImage';
+import { fetchImagesForGallery } from "@/lib/data.service";
+import styles from "./page.module.css";
+import DevDebugJson from "@/components/dev/devDebugJson/devDebugJson";
+import DevGalleryImage from "@/components/dev/devGalleryImage/devGalleryImage";
 
 const Page = async () => {
+  let gallery = "obscura";
+  const images = await fetchImagesForGallery(gallery);
 
-    let gallery = 'umbra';
-    const images = await fetchImagesForGallery(gallery);
+  return (
+    <main className={styles.page}>
+      <h1>Alle Billeder Fra {gallery} Data</h1>
+      <DevDebugJson content={images}></DevDebugJson>
 
-    return (
-        <main className={styles.page}>
-            <h1>Alle Billeder Fra {gallery} Data</h1>
-            <DevDebugJson content={images}></DevDebugJson>
+      <h1>Hvert Enkelt Billede</h1>
+      {images.map((image, index) => {
+        return <DevGalleryImage key={index} image={image} />;
+      })}
+    </main>
+  );
+};
 
-            <h1>Hvert Enkelt Billede</h1>
-            {images.map((image, index) => {
-                return <DevGalleryImage key={index} image={image} />
-            })}
-        </main>
-    );
-
-}
-
-export default Page
+export default Page;
